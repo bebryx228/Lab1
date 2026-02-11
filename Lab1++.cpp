40c587caf5ad927e15b066c6138b85ce7ad09b28
bebryx228 <geergee83@gmail.com>
2026-02-11 23:33:21 +0300

    Add files via upload

---
+++ Lab1++.cpp
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <iostream>
+
+int shirEuklid( int a , int b , int &x , int &y)
+{
+    if (a == 0)
+    {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1 , y1;
+    int c = shirEuklid(b % a , a , x1 , y1);
+
+    x = y1 - (b / a)*x1;
+    y = x1;
+    return c;
+}
+int main()
+{
+    int a = 3  , b = 2 , c , x , y;
+    //std::cout << "Введите а и b";
+    //std::cin >> a >> b ;
+    c = shirEuklid(a , b , x ,y);
+    std::cout << c << "\t" << x << "\t" << y << std::endl;
+    std::cout << "Резников Герман" << std::endl;
+    return 0;
+}
